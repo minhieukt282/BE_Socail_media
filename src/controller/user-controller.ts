85a7e850a52dc3a4eb60bbf27ac684d1091e5c0f
minhieukt282 <minhieukt@gmail.com>
2022-12-28 11:30:47 +0700

    built: friend relationship
    thêm controller

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -9,7 +9,27 @@ export class UserController {
     }
 
     showFriends = async (req: Request, res: Response): Promise<string | any> => {
-        let respBody = await this.userService.getFriends(req.params.username)
+        let respBody = await this.userService.showFriends(+req.params.accountId)
+        return res.status(respBody.code).json(respBody)
+    }
+
+    makeFriend = async (req: Request, res: Response): Promise<string | any> => {
+        let respBody = await this.userService.makeFriend(req.body)
+        return res.status(respBody.code).json(respBody)
+    }
+
+    waitingFriends = async (req: Request, res: Response): Promise<string | any> => {
+        let respBody = await this.userService.waitingFriends(+req.params.accountRes)
+        return res.status(respBody.code).json(respBody)
+    }
+
+    acceptFriend = async (req: Request, res: Response): Promise<string | any> => {
+        let respBody = await this.userService.acceptFriend(+req.params.relationshipId)
+        return res.status(respBody.code).json(respBody)
+    }
+
+    declineFriend = async (req: Request, res: Response): Promise<string | any> => {
+        let respBody = await this.userService.declineFriend(+req.params.relationshipId)
         return res.status(respBody.code).json(respBody)
     }
 }
