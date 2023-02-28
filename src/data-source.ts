b6ba97391c73fefbbca1282148179e6184111d20
Cikie <daoanh123123@gmail.com>
2023-02-28 22:29:34 +0700

    ready to merge

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,8 +12,8 @@ export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "anhnbt",
-    password: "KhoaiTay@2019",
+    username: "root",
+    password: "123456",
     database: "social_media",
     synchronize: false,
     entities: [
